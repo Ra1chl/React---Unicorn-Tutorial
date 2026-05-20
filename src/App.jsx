f509099b5b73cdb34ef0bf1f4e37cb111a3ec139
Ra1chl <raichljonas1@gmail.com>
2026-05-20 10:38:22 +0200

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Message from './Message'
+import ListGroup from './components/ListGroup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
-  )
+
+    <><Message></Message><ListGroup></ListGroup></>
+  );
+
 }
 
 export default App
---
+++ src/components/ListGroup.jsx
@@ -0,0 +1,17 @@
+function ListGroup() {
+  return (
+    <>
+      <h1>List Group</h1>
+
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
